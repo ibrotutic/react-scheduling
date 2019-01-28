76cdeea2568f9a0e9a8a4fc91681df95cc393014
Jacob Veatch <jakedveatch@gmail.com>
2019-01-27 19:31:42 -0600

    Created side drawer for user account

---
+++ Experiments/Jake/Frontend/bookingapp/src/components/navbar-drawer.js
@@ -0,0 +1,90 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Avatar from "@material-ui/core/Avatar";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Today from "@material-ui/icons/CalendarTodaySharp";
+import Search from "@material-ui/icons/Search";
+import Alarm from "@material-ui/icons/Alarm";
+import Settings from "@material-ui/icons/Settings";
+
+const styles = {
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
+  }
+};
+
+class NavbarDrawer extends React.Component {
+  state = {
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  };
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    const sideList = (
+      <div className={classes.list}>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <Avatar>?</Avatar>
+            </ListItemIcon>
+            <ListItemText primary="Account" />
+          </ListItem>
+          <Divider />
+          {["Search", "My Calendar", "Appointments", "Settings"].map(
+            (text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index === 0 ? (
+                    <Search />
+                  ) : index === 1 ? (
+                    <Today />
+                  ) : index === 2 ? (
+                    <Alarm />
+                  ) : (
+                    <Settings />
+                  )}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            )
+          )}
+        </List>
+      </div>
+    );
+
+    return (
+      <div>
+        <Drawer open={this.props.show} onClose={() => this.props.close()}>
+          <div tabIndex={0} role="button">
+            {sideList}
+          </div>
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+NavbarDrawer.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(NavbarDrawer);
--- Experiments/Jake/Frontend/bookingapp/src/components/navbar.js
+++ Experiments/Jake/Frontend/bookingapp/src/components/navbar.js
@@ -1,74 +1,24 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
-
-const styles = {
-  root: {
-    flexGrow: 1
-  },
-  grow: {
-    flexGrow: 1,
-    textAlign: "left"
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  }
-};
+import ButtonAppBar from "./button-app-bar";
+import NavbarDrawer from "./navbar-drawer";
 
 class Navbar extends Component {
-  state = {};
+  state = {
+    drawer: false
+  };
+
   render() {
     return (
       <div>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="Menu"
-              onClick={() => this.setState({ openDrawer: true })}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h4" color="inherit">
-              BookingRUs
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-          <Drawer
-            open={this.state.openDrawer}
-            onClose={() => this.setState({ openDrawer: false })}
-          >
-            <div
-              tabIndex={0}
-              role="button"
-              onClick={() => this.setState({ openDrawer: false })}
-              onKeyDown={() => this.setState({ openDrawer: false })}
-            >
-              <List>
-                <ListItem button key={1}>
-                  <ListItemIcon>
-                    {4 % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={"test"} />
-                </ListItem>
-              </List>
-            </div>
-          </Drawer>
-        </AppBar>
+        <NavbarDrawer
+          show={this.state.drawer}
+          close={() => this.setState({ drawer: false })}
+        />
+        <ButtonAppBar
+          menuClick={() => {
+            this.setState({ drawer: true });
+          }}
+        />
       </div>
     );
   }
