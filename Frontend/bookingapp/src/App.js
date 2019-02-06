6c86fc965345abaee0dc96041b6158578ddf2981
Jacob Veatch <jakedveatch@gmail.com>
2019-02-05 22:46:18 -0600

    Added amplify library and wrapped app with AmplifyHOC

--- Frontend/bookingapp/src/App.js
+++ Frontend/bookingapp/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
-import NavController from "./pages/nav-controller"
+import NavController from "./pages/nav-controller";
+import Amplify, { Auth } from "aws-amplify";
+import awsmobile from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
+
+Amplify.configure(awsmobile);
 
 class App extends Component {
-
-    render() {
-        return (
-            <div className="App">
-                <Navbar />
-                <NavController/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <NavController />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App);
