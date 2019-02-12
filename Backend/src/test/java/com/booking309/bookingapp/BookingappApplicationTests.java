58d91ad68a5f14246462358fecc8df955ee7dfd6
Jacob Veatch <jakedveatch@gmail.com>
2019-02-12 13:33:01 -0600

    Init backend after git ignore

---
+++ Backend/src/test/java/com/booking309/bookingapp/BookingappApplicationTests.java
@@ -0,0 +1,17 @@
+package com.booking309.bookingapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookingappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
