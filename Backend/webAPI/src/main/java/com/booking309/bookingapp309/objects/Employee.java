46715d9df335eeb4e5dd94dcf4522831737beff2
Ibro Tutic <ibro@Ibros-MacBook-Pro.local>
2019-02-16 11:57:03 -0600

    updated docker config

---
+++ Backend/webAPI/src/main/java/com/booking309/bookingapp309/objects/Employee.java
@@ -0,0 +1,11 @@
+package com.booking309.bookingapp309.objects;
+
+public class Employee {
+
+    private String emloyeeName;
+    private String employeeId;
+
+    public Employee(StremployeeName, employeeId) {
+
+    }
+}
