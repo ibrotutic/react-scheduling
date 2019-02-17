d6d72e69bc60f5bdccacce17c262958cc6d4a2d0
Hunter Hall <Hunter Hall>
2019-02-17 17:28:39 -0600

    Now it'd finished finished

--- Backend/webAPI/src/main/java/com/booking309/bookingapp309/objects/Person.java
+++ Backend/webAPI/src/main/java/com/booking309/bookingapp309/objects/Person.java
@@ -2,6 +2,7 @@ package com.booking309.bookingapp309.objects;
 
 public class Person extends com.booking309.bookingapp309.objects.Response {
 
+    private final int pid;
     private final String firstname;
     private final String lastname;
     private final String email;
@@ -13,11 +14,12 @@ public class Person extends com.booking309.bookingapp309.objects.Response {
     private final String businesstype;
 
 
-    public Person(int respCode, String firstname, String lastname, String email, String password,
+    public Person(int respCode, int pid, String firstname, String lastname, String email, String password,
                   String employer, int empid, int adminid, String businessname, String businesstype) {
 
         super(respCode);
 
+        this.pid = pid;
         this.firstname = firstname;
         this.lastname = lastname;
         this.email = email;
@@ -29,6 +31,10 @@ public class Person extends com.booking309.bookingapp309.objects.Response {
         this.businesstype = businesstype;
     }
 
+    public int getPid(){
+        return pid;
+    }
+    
     public String getFirstname() {
         return firstname;
     }
