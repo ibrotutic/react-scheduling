b3c2e9f1e966c5271d87a6730315752eb809b0fb
Seth Jones <sethj@iastate.edu>
2019-02-18 23:31:22 -0600

    forgot to change one part

--- Backend/webAPI/src/main/java/com/booking309/bookingapp309/controllers/OrgController.java
+++ Backend/webAPI/src/main/java/com/booking309/bookingapp309/controllers/OrgController.java
@@ -1,7 +1,6 @@
 package com.booking309.bookingapp309.controllers;
 
 import com.booking309.bookingapp309.objects.ClientError;
-import com.booking309.bookingapp309.objects.EmployeeList;
 import com.booking309.bookingapp309.objects.Organization;
 import com.booking309.bookingapp309.objects.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
