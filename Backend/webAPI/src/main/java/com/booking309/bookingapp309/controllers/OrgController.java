fdc3c91d008946bc07de6de77d691886f17fe770
Jacob Veatch <jakedveatch@gmail.com>
2019-02-20 14:16:52 -0600

    Optimized imports

--- Backend/webAPI/src/main/java/com/booking309/bookingapp309/controllers/OrgController.java
+++ Backend/webAPI/src/main/java/com/booking309/bookingapp309/controllers/OrgController.java
@@ -1,15 +1,9 @@
 package com.booking309.bookingapp309.controllers;
 
-import com.booking309.bookingapp309.objects.ClientError;
 import com.booking309.bookingapp309.objects.Organization;
-import com.booking309.bookingapp309.objects.Response;
 import com.booking309.bookingapp309.repositories.OrgRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashSet;
-import java.util.Map;
 
 
 @RestController
--- Backend/webAPI/src/main/java/com/booking309/bookingapp309/repositories/OrgRepository.java
+++ Backend/webAPI/src/main/java/com/booking309/bookingapp309/repositories/OrgRepository.java
@@ -3,8 +3,6 @@ package com.booking309.bookingapp309.repositories;
 import com.booking309.bookingapp309.objects.Organization;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface OrgRepository extends CrudRepository<Organization, String> {
 
     Organization findById(Integer id);
