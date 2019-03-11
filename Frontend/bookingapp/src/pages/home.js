d782914acb26818c25121d042946bf3eef8ad30f
Ibro Tutic <ibro@ibros-mbp.student.iastate.edu>
2019-03-11 15:01:07 -0500

    just a search bar on home page

--- Frontend/bookingapp/src/pages/home.js
+++ Frontend/bookingapp/src/pages/home.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+import Background from "../background.jpg";
+import Search from "../components/search";
+import Icon from "../icon.svg";
+import LoginCard from "../components/login-card";
+import SignUpCard from "../components/signup-card";
+import { Auth } from "aws-amplify";
 
-const Home = () => (
-    <h1>Home stub.</h1>
-);
+var styles = {
+  background: {
+    backgroundImage: `url(${Background})`,
+    flex: "1",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    height: "800px",
+    width: "100%",
+    textAlign: "center",
+    alignItems: "center"
+  },
+  header: {
+    height: "30%"
+  },
+  logo: {
+    minHeight: "200px"
+  },
+  search: {
+    display: "flex",
+    justifyContent: "center"
+  }
+};
 
-export default Home
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={styles.background}>
+        <div>
+          <div style={styles.header}>
+            <img src={Icon} alt="My logo" style={styles.logo} />
+          </div>
+          <div style={styles.search}>
+            <Search />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
