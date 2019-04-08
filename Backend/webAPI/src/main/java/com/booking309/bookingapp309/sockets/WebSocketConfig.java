ca6179b33f17a4ae94c7cc646f6754f44ddd87a9
Jacob Veatch <jakedveatch@gmail.com>
2019-04-08 14:23:23 -0500

    Added website allowed endpoint

--- Backend/webAPI/src/main/java/com/booking309/bookingapp309/sockets/WebSocketConfig.java
+++ Backend/webAPI/src/main/java/com/booking309/bookingapp309/sockets/WebSocketConfig.java
@@ -12,7 +12,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/appointment").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/appointment").setAllowedOrigins("http://localhost:3000",
+                "http://cs309-pp-7.misc.iastate.edu").withSockJS();
     }
 
     @Override
