09ad1346e2983d1ccee8fb4b514bb2db90f33194
Jacob Veatch <jakedveatch@gmail.com>
2019-04-27 12:16:45 -0500

    Updated api docs to have photos

--- Frontend/bookingapp/src/components/modals/organization-scheduling-modal.js
+++ Frontend/bookingapp/src/components/modals/organization-scheduling-modal.js
@@ -175,7 +175,9 @@ class OrganizationSchedulingModal extends Component {
           </div>
         );
       case "gallery":
-              return <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+        return (
+          <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        );
       case "ratings":
       default:
         return <div>{selectedTab}</div>;
